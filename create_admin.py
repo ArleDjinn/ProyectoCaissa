31b329e5c07fe6e443db1c3451f4faca28617cbf
ArleDjinn <ignacio.carrere@gmail.com>
2025-10-02 12:23:23 -0300

    Arreglo errores

---
+++ create_admin.py
@@ -0,0 +1,21 @@
+# create_admin.py
+from app import create_app, db
+from app.models import User
+
+app = create_app()
+
+with app.app_context():
+    # Verificar si ya existe
+    existing = User.query.filter_by(email="batman@caissa.cl").first()
+    if existing:
+        print("⚠️ El usuario admin ya existe:", existing)
+    else:
+        admin = User(
+            email="batman@caissa.cl",
+            name="Batman",
+            is_admin=True
+        )
+        admin.set_password("IamBatman")
+        db.session.add(admin)
+        db.session.commit()
+        print("✅ Admin Batman creado correctamente.")
